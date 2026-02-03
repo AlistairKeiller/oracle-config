c81443dc8d12fb7085c7be42b14821b3d631d4fa
Alistair Keiller <alistair@keiller.net>
2026-02-03 15:53:45 -0800

    first commit

---
+++ setup.sh
@@ -0,0 +1,40 @@
+# Getting alacritty to work
+echo 'export TERM=xterm-256color' >> ~/.bashrc
+
+# Systemd
+mkdir -p /etc/systemd/system
+cp -r ./system/* /etc/systemd/system/
+
+# Pufferpanel
+curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh?any=true | sudo bash
+sudo apt update
+sudo apt-get install pufferpanel
+sudo pufferpanel user add
+sudo systemctl enable --now pufferpanel
+
+# Clear all kernel firewall rules
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -F
+sudo iptables -X
+sudo iptables -Z
+sudo ip6tables -P INPUT ACCEPT
+sudo ip6tables -P FORWARD ACCEPT
+sudo ip6tables -P OUTPUT ACCEPT
+sudo ip6tables -F
+sudo ip6tables -X
+sudo ip6tables -Z
+
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install git
+sudo apt update
+sudo apt install git
+
+# build Kokoros
+sudo apt install build-essential libssl-dev pkg-config cmake libclang-dev libopus-dev libsonic-dev libpcaudio-dev
+git clone https://github.com/lucasjinreal/Kokoros
+cd Kokoros
+cargo build --release
